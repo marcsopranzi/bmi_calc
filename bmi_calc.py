0b07739fa4834968985f69f0b058c30ab41e4c22
Marcelo <marcsopranzi@gmail.com>
2023-03-30 06:02:39 +0100

    removed unsued libraries

--- bmi_calc.py
+++ bmi_calc.py
@@ -1,36 +1,36 @@
-
-from operator import index
-from unicodedata import decimal
 import pandas as pd
 import sqlite3
-import math 
 
 
-bmi_path = 'data/table1.json'
-category_path = 'data/catalog.csv'
+bmi_path = "data/table1.json"
+category_path = "data/catalog.csv"
 conn = sqlite3.connect(":memory:")
 
-def sqlite_power(x,n):
+
+def sqlite_power(x, n):
     return x**n
+
 
 conn.create_function("power", 2, sqlite_power)
 
 df_bmi_data = pd.read_json(bmi_path)
-df_category_data = pd.read_csv(category_path, sep='\t')
+df_category_data = pd.read_csv(category_path, sep="\t")
 
 df_bmi_data.to_sql("table_bmi_data", conn, index=False)
 df_category_data.to_sql("table_category_data", conn, index=False)
 
-qry_bmi_calculation = """select *, 
+qry_bmi_calculation = """select *,
                                 case when WeightKg > 0 and HeightCm > 0
-                                then WeightKg/power(cast(HeightCm as real)/100, 2) 
-                                else 0 
-                                end as BMI_index 
+                                then WeightKg/power(cast(HeightCm as real)/100, 2)
+                                else 0
+                                end as BMI_index
                          from table_bmi_data"""
 
+
 def bmi_calculation(df):
-    df = pd.read_sql_query(qry_bmi_calculation,conn)
+    df = pd.read_sql_query(qry_bmi_calculation, conn)
     return df
+
 
 df_bmi_data_bmi = bmi_calculation(df_bmi_data)
 
@@ -41,13 +41,15 @@ qry_bmi_categorization = """select Gender,
                                    WeightKg,
                                    bmi_category bmi_range_kg_m2,
                                    health_risk
-                            from table_bmi_data_bmi 
-                            join table_category_data on table_bmi_data_bmi.BMI_index > table_category_data.bmimin and 
+                            from table_bmi_data_bmi
+                            join table_category_data on table_bmi_data_bmi.BMI_index > table_category_data.bmimin and
                                  table_bmi_data_bmi.BMI_index <  table_category_data.bmimax"""
+
 
 def bmi_categorization(df):
     df = pd.read_sql_query(qry_bmi_categorization, conn)
     return df
+
 
 df_bmi_data_category = bmi_categorization(df_bmi_data_bmi)
 
@@ -56,15 +58,16 @@ print(df_bmi_data_category)
 df_bmi_data_category.to_sql("table_bmi_data_category", conn, index=False)
 
 qry_category_overweight_count = """select count(*) as OverWeight_people
-                                   from table_bmi_data_category 
+                                   from table_bmi_data_category
                                    where bmi_range_kg_m2 = 'Overweight'"""
+
 
 def bmi_category_overweight_count(df):
     df = pd.read_sql_query(qry_category_overweight_count, conn)
     return df
 
+
 df_bmi_category_overweight_count = bmi_category_overweight_count(df_bmi_data_category)
 
 
 print(df_bmi_category_overweight_count)
-
--- setup_env.sh
+++ setup_env.sh
@@ -13,7 +13,7 @@ echo "Activating venv"
 source ./venv_bmi_calc/bin/activate
 
 echo "Install dependencies"
-pip install "pytest==7.1.2" "numpy==1.23.0" "pandas" "math" "sqlite3"
+pip install "pytest==7.1.2" "numpy==1.23.0" "pandas==1.4.3" "pre-commit"
 
 echo "Pip upgrade"
 pip install --upgrade pip
